bad344a7b9920c392033961c076a690f6c696f87
jakeinman05 <jakeinman05@gmail.com>
2025-01-20 14:22:53 -0500

    Changes to Blade class

--- src/main/java/com/notvergin/jmr/customitems/weapons/ImmortalBlade.java
+++ src/main/java/com/notvergin/jmr/customitems/weapons/ImmortalBlade.java
@@ -1,8 +1,19 @@
 package com.notvergin.jmr.customitems.weapons;
 
+import net.minecraft.nbt.TextComponentTagVisitor;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+import java.util.List;
 
 public class ImmortalBlade extends SwordItem
 {
@@ -11,9 +22,25 @@ public class ImmortalBlade extends SwordItem
     }
 
     @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced)
+    {
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+        pTooltipComponents.add(Component.literal("Unbreakable")
+                .withStyle(style -> style.withColor(TextColor.fromRgb(0xeb4034))));
+    }
+
+    @Override
     public boolean isDamageable(ItemStack stack) {
         return false;
     }
     @Override
     public boolean isFoil(ItemStack stack) { return true; }
+
+    // ideas
+
+    // right click:
+    //
+
+    // maybe implement tired sword
+    // can also use framework for later mods????
 }
