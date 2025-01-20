2fa05b747daf6e6a12cdc128691cf734c59314b9
jakeinman05 <jakeinman05@gmail.com>
2025-01-20 14:24:47 -0500

    Registry for shard

--- src/main/java/com/notvergin/jmr/registries/ModItems.java
+++ src/main/java/com/notvergin/jmr/registries/ModItems.java
@@ -1,9 +1,10 @@
 package com.notvergin.jmr.registries;
 
 import com.notvergin.jmr.customitems.item.ImmortalGem;
+import com.notvergin.jmr.customitems.item.ImmortalShard;
+import com.notvergin.jmr.customitems.weapons.ImmortalBlade;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,10 +21,14 @@ public class ModItems
     // Items
     public static final RegistryObject<Item> JOHN_TEAR = ITEMS.register("john_tear",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IMMORTALITY_SHARD = ITEMS.register("immortality_shard",
+            () -> new ImmortalShard(new Item.Properties()));
     public static final RegistryObject<Item> IMMORTALITY_GEM = ITEMS.register("immortality_gem",
             () -> new ImmortalGem(new Item.Properties().rarity(Rarity.RARE)));
+
+    // Tools
     public static final RegistryObject<Item> IMMORTALITY_SWORD = ITEMS.register("immortality_sword",
-            () -> new SwordItem(Tiers.DIAMOND, 3, -2.4F, new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+            () -> new ImmortalBlade(Tiers.DIAMOND, 3, -2.4F, new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
 
     public static void register(IEventBus bus)
     {
