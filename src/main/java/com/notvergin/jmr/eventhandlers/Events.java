3dfb58f7a93ca0b041433d476fd77aa9b1fac74d
jakeinman05 <jakeinman05@gmail.com>
2025-01-21 13:04:38 -0500

    Added avoid goal to Villager

--- src/main/java/com/notvergin/jmr/eventhandlers/Events.java
+++ src/main/java/com/notvergin/jmr/eventhandlers/Events.java
@@ -1,7 +1,11 @@
 package com.notvergin.jmr.eventhandlers;
 
 import com.notvergin.jmr.customitems.weapons.ImmortalBlade;
+import com.notvergin.jmr.entity.mobs.JohnEntity;
+import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,4 +22,14 @@ public class Events
 //        Entity entity = event.getEntity();
 //        Minecraft.getInstance().particleEngine.createParticle(ParticleTypes.LAVA, entity.getX(), entity.getY(), entity.getZ(), 0, 0, 0);
 //    }
+
+    @SubscribeEvent
+    public static void onVillagerSpawn(EntityJoinLevelEvent event)
+    {
+        if(event.getEntity() instanceof Villager villager)
+        {
+            villager.goalSelector.addGoal(1, new AvoidEntityGoal<>(villager, JohnEntity.class, 16.0f, 1.0f, 1.3f));
+        }
+    }
+
 }
