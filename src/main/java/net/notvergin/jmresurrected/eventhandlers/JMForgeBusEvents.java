b6ff2604d0dbbd4ea873a72f294ded08349209ed
jakeinman05 <jakeinman05@gmail.com>
2025-02-19 02:05:33 -0500

    Added Perennial feature

--- src/main/java/net/notvergin/jmresurrected/eventhandlers/JMForgeBusEvents.java
+++ src/main/java/net/notvergin/jmresurrected/eventhandlers/JMForgeBusEvents.java
@@ -1,8 +1,16 @@
 package net.notvergin.jmresurrected.eventhandlers;
 
-import net.notvergin.jmresurrected.customitems.JMModItems;
-import net.notvergin.jmresurrected.customitems.weapons.ImmortalBlade;
-import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.notvergin.jmresurrected.blocks.ImmortalGemBlock;
+import net.notvergin.jmresurrected.blocks.JMBlocks;
+import net.notvergin.jmresurrected.blocks.PerennialBlock;
+import net.notvergin.jmresurrected.items.JMItems;
+import net.notvergin.jmresurrected.items.weapons.ImmortalBlade;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
@@ -16,6 +24,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import static net.notvergin.jmresurrected.JohnModResurrected.MODID;
+import static net.notvergin.jmresurrected.blocks.PerennialBlock.ACTIVE;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class JMForgeBusEvents
@@ -23,20 +32,21 @@ public class JMForgeBusEvents
     @SubscribeEvent
     public static void onRefGemUse(PlayerInteractEvent.RightClickItem event)
     {
-        if(!event.getEntity().level().isClientSide)
-        {
-            Player player = event.getEntity();
+        Player player = event.getEntity();
 
+        if(!player.level().isClientSide)
+        {
             ItemStack mainHand = player.getMainHandItem();
             ItemStack offHand = player.getOffhandItem();
 
-            if(offHand.getItem() == JMModItems.REFINED_IMMORTAL_GEM.get() && !mainHand.isEmpty())
+            if(offHand.getItem() == JMItems.REFINED_IMMORTAL_GEM.get() && !mainHand.isEmpty())
             {
-                if(mainHand.getItem() == JMModItems.IMMORTALITY_SWORD.get())
+                if(mainHand.getItem() == JMItems.IMMORTALITY_SWORD.get())
                 {
                     doHandEffects(player);
                     mainHand.shrink(1);
-                    player.setItemInHand(InteractionHand.MAIN_HAND, JMModItems.UIMMORTALITY_SWORD.get().getDefaultInstance());
+                    player.setItemInHand(InteractionHand.MAIN_HAND, JMItems.UIMMORTALITY_SWORD.get().getDefaultInstance());
+                    event.setCancellationResult(InteractionResult.CONSUME);
                     event.setCanceled(true);
                     return;
                 }
@@ -45,6 +55,7 @@ public class JMForgeBusEvents
                 {
                     player.swing(InteractionHand.OFF_HAND);
                     player.sendSystemMessage(Component.literal("Item is already unbreakable."));
+                    event.setCancellationResult(InteractionResult.CONSUME);
                     event.setCanceled(true);
                     return;
                 }
@@ -55,12 +66,46 @@ public class JMForgeBusEvents
                     tag.putBoolean("Unbreakable", true);
                     mainHand.setTag(tag);
                     doHandEffects(player);
-                    event.setCanceled(true);
                 }
                 else {
                     player.swing(InteractionHand.OFF_HAND);
                     player.sendSystemMessage(Component.literal("Item cannot be used with this."));
-                    event.setCanceled(true);
+                }
+                event.setCancellationResult(InteractionResult.CONSUME);
+                event.setCanceled(true);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void PlayerRightClickGemBlock(PlayerInteractEvent.RightClickBlock event) {
+        Player player = event.getEntity();
+        Level level = player.level();
+        BlockState state = level.getBlockState(event.getPos());
+        BlockPos pos = event.getPos();
+
+        if(!level.isClientSide)
+        {
+            Item handItem = player.getMainHandItem().getItem();
+            Block clickedBlock = state.getBlock();
+
+            if(clickedBlock instanceof ImmortalGemBlock gemBlock && !player.isShiftKeyDown()) {
+                if(handItem == JMItems.REFINED_IMMORTAL_GEM.get()) {
+                    player.swing(InteractionHand.MAIN_HAND);
+
+                    if(!player.isCreative()) {
+                        player.getMainHandItem().shrink(1);
+                    }
+
+                    gemBlock.destroy(level, event.getPos(), state);
+
+                    ((ServerLevel) level).sendParticles(ParticleTypes.DRAGON_BREATH, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 32, 0.2D, 0.3D, 0.2D, 0.1D );
+                    //level.playSound(null, pos, SoundEvents.WITHER_SPAWN, SoundSource.BLOCKS, 0.3F, 0.3F);
+
+                    BlockState perennial = JMBlocks.PERENNIAL_BLOCK.get().defaultBlockState();
+                    level.setBlockAndUpdate(pos, perennial);
+
+                    event.setCancellationResult(InteractionResult.CONSUME);
                 }
             }
         }
@@ -68,18 +113,13 @@ public class JMForgeBusEvents
 
     private static void doHandEffects(Player player)
     {
-        Minecraft.getInstance().particleEngine.createTrackingEmitter(player, ParticleTypes.DRAGON_BREATH);
-        player.swing(InteractionHand.MAIN_HAND);
-        player.swing(InteractionHand.OFF_HAND);
-        player.level().playLocalSound(
-                player.getX(),
-                player.getY(),
-                player.getZ(),
-                SoundEvents.ANVIL_BREAK,
-                SoundSource.PLAYERS,
-                1.0f,
-                1.0f,
-                true);
-        player.getOffhandItem().shrink(1);
+        if(!player.level().isClientSide) {
+            player.swing(InteractionHand.MAIN_HAND);
+            player.swing(InteractionHand.OFF_HAND);
+            player.getOffhandItem().shrink(1);
+            player.level().playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ANVIL_DESTROY, SoundSource.PLAYERS, 1.0F, 1.0F);
+
+            ((ServerLevel) player.level()).sendParticles(ParticleTypes.DRAGON_BREATH, player.getX(), player.getY() + 1, player.getZ(), 16, 0.3, 0.3, 0.3, 0.2);
+        }
     }
 }
