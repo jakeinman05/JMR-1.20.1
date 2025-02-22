3d0803fe661332b4318620bc7a8725e21047e926
jakeinman05 <jakeinman05@gmail.com>
2025-02-21 19:14:36 -0500

    Added creative tab

--- src/main/java/net/notvergin/jmresurrected/JohnModResurrected.java
+++ src/main/java/net/notvergin/jmresurrected/JohnModResurrected.java
@@ -1,5 +1,10 @@
 package net.notvergin.jmresurrected;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 import net.notvergin.jmresurrected.blocks.JMBlocks;
 import net.notvergin.jmresurrected.blocks.blockentities.JMBlockEntities;
 import net.notvergin.jmresurrected.mobeffects.RegisterBrewingRecipe;
@@ -17,11 +22,34 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(JohnModResurrected.MODID)
 public class JohnModResurrected
 {
     public static final String MODID = "jmresurrected";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public static final DeferredRegister<CreativeModeTab> TAB_REGISTER = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final RegistryObject<CreativeModeTab> JM_CREATIVE_TAB = TAB_REGISTER.register("jm_creative_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .title(Component.literal("John Mod Resurrected"))
+            .icon(() -> JMItems.IMMORTALITY_GEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(JMItems.IMMORTALITY_SHARD.get());
+                output.accept(JMItems.IMMORTALITY_GEM.get());
+                output.accept(JMItems.JOHN_TEAR.get());
+                output.accept(JMItems.IMMORTAL_HUSK.get());
+                output.accept(JMItems.REFINED_IMMORTAL_GEM.get());
+                output.accept(JMBlocks.IMMORTAL_GEM_BLOCK.get());
+                output.accept(JMBlocks.PERENNIAL_BLOCK.get());
+                output.accept(JMItems.IMMORTALITY_SWORD.get());
+                output.accept(JMItems.UIMMORTALITY_SWORD.get());
+                output.accept(JMItems.BABY_JOHN_EGG.get());
+                output.accept(JMItems.JOHN_EGG.get());
+            }).build());
 
     public JohnModResurrected() {}
 
@@ -37,37 +65,15 @@ public class JohnModResurrected
         JMPotionRegistry.register(modEventBus);
         JMSounds.register(modEventBus);
 
-        modEventBus.addListener(this::commonSetup);
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        TAB_REGISTER.register(modEventBus);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         RegisterBrewingRecipe.registerCustomBrewingRecipes();
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(JMItems.JOHN_TEAR);
-            event.accept(JMItems.IMMORTALITY_GEM);
-            event.accept(JMItems.IMMORTALITY_SHARD);
-            event.accept(JMItems.IMMORTAL_HUSK);
-        }
-        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-            event.accept(JMItems.REFINED_IMMORTAL_GEM);
-        }
-        if(event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
-            event.accept(JMItems.JOHN_EGG);
-            event.accept(JMItems.BABY_JOHN_EGG);
-        }
-        if(event.getTabKey() == CreativeModeTabs.COMBAT) {
-            event.accept(JMItems.IMMORTALITY_SWORD);
-            event.accept(JMItems.UIMMORTALITY_SWORD);
-        }
     }
 
     @SubscribeEvent
